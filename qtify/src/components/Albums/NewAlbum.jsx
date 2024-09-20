58071f978cb6e2dd96d73e1da2bcafc9ee7aacee
Prince Prabhat <princeprabhat1996@gmail.com>
2024-09-20 23:50:52 +0530

    Add() Carousel structure

--- qtify/src/components/Albums/NewAlbum.jsx
+++ qtify/src/components/Albums/NewAlbum.jsx
@@ -16,22 +16,22 @@ const NewAlbum = ({ albumData, title }) => {
       <Section
         title={title}
         onToggle={toggleGrid}
-        btnText={isToggled ? "Show all" : "Collapse"}
+        btnText={!isToggled ? "Show all" : "Collapse"}
       />
-      {isToggled ? (
+      {!isToggled ? (
         <Carousel data={albumData} />
       ) : (
         <Grid2
           container
           justifyContent="space-between"
-          spacing={4}
+          spacing={2}
           marginBottom="30px"
         >
           {albumData.length > 0 &&
             albumData.map((data) => {
               return (
                 <Grid2
-                  // size={{ sm: 4, md: 3, lg: 12 / 5, xl: 2 }}
+                  // size={{ sm: 4, md: 3, lg: 12 / 5, xl: 1 }}
                   key={data.id}
                 >
                   <Cards data={data} />
--- qtify/src/components/Carousel/Carousel.jsx
+++ qtify/src/components/Carousel/Carousel.jsx
@@ -2,20 +2,36 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import Cards from "../Cards/Cards";
+import styles from "./Carousel.module.css";
 
 const Carousel = ({ data }) => {
+  const width = window.innerWidth;
+  console.log(width);
   return (
-    <div style={{ background: "white", height: "30vh" }}>
+    <div className={styles.carousel_container} style={{ height: "30vh" }}>
       {data.length && (
         <Swiper
-          spaceBetween={50}
-          slidesPerView={3}
           onSlideChange={() => console.log("slide change")}
           onSwiper={(swiper) => console.log(swiper)}
+          className={styles.swiper_container}
+          spaceBetween={15} // Space between cards
+          slidesPerView={1} // Initial view of cards
+          breakpoints={{
+            566: { slidesPerView: 3 }, // 1 card on small screens
+            760: { slidesPerView: 4 }, // 2 cards on medium screens
+            955: { slidesPerView: 5 }, // 3 cards on large screens
+            1149: { slidesPerView: 6 },
+            1344: { slidesPerView: 7 },
+            1538: { slidesPerView: 8 },
+            1733: { slidesPerView: 9 },
+            1927: { slidesPerView: 10 },
+            2122: { slidesPerView: 11 },
+            2316: { slidesPerView: 12 },
+          }}
         >
           {data.map((cardData) => {
             return (
-              <SwiperSlide>
+              <SwiperSlide className={styles.swiper_slide}>
                 <Cards data={cardData} />
               </SwiperSlide>
             );
